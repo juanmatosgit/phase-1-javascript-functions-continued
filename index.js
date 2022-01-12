03156f81480f3039417d4f6a2b78ac68f8a6c5b8
Juan <ssjmatos@gmail.com>
2022-01-12 16:56:28 -0500

    commit tree be00ab45a50fac036248138b821a374e6b4a097b

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // code your solution here
+
+function saturdayFun (activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+const mondayWork = function (activity = "go to the office") {
+    return `This Monday, I will ${activity}.`
+  }
+
+function wrapAdjective (asterik) {
+    return function (xyz = "special") {
+        return `You are ${asterik}${xyz}${asterik}!`
+    }
+}
+
+
+//   function demoChain(name) {
+//     const pLevel = "Power level over 9000"
+//     return function () {
+//       const dbzName = "Vegeta"
+//       return function () { 
+//         console.log(`Who is ${name}? ${dbzName}, "His ${pLevel}!"`)
+//       }
+//     }
+//   }
+  
+//   demoChain("Goku")()();
